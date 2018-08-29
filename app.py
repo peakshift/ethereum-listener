ad0b46d2ab0cdf04da60238bf952c9802d8666de
micey969 <micey_969@hotmail.com>
2018-08-29 15:07:58 -0400

    ci(circleci): setup circlci

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import os
 from web3 import Web3
-from utils.events import *
+from utils.events import log_loop
 
 
 url = os.environ['server']  # blockchain server
@@ -8,20 +8,18 @@ w3 = Web3(Web3.HTTPProvider(url))
 
 
 def main():
-	"""
-	This functions takes asks for address
-	and creates and event listener to watch
-	for the inputted address
-	"""
-	address = input("Enter address to search for: ")
-	if w3.isAddress(address):
-		block_filter = w3.eth.filter('latest')
-		log_loop(block_filter, 2, address, w3)
-	else:
-		main()
+    """
+    This functions takes asks for address
+    and creates and event listener to watch
+    for the inputted address
+    """
+    address = input("Enter address to search for: ")
+    if w3.isAddress(address):
+        block_filter = w3.eth.filter('latest')
+        log_loop(block_filter, 2, address, w3)
+    else:
+        main()
 
 
 if __name__ == '__main__':
-	main()
-	
-
+    main()
--- utils/database.py
+++ utils/database.py
@@ -2,40 +2,40 @@ from tinydb import TinyDB
 
 
 class Database:
-	def __init__(self):
-		self.db = TinyDB('./db.json')
+    def __init__(self):
+        self.db = TinyDB('./db.json')
 
-	def insert(self, collection: str, data: dict):
-		try:
-			id: int = self.db.table(collection).insert(data)
-			return True
-		except:
-			return None
+    def insert(self, collection: str, data: dict) -> bool:
+        try:
+            project_id: int = self.db.table(collection).insert(data)
+            return True
+        except:
+            return None
 
-	def get(self, collection: str):
-		try:
-			projects: list = self.db.table(collection).all()
-			return projects
-		except:
-			return None
+    def get(self, collection: str) -> list:
+        try:
+            projects: list = self.db.table(collection).all()
+            return projects
+        except:
+            return None
 
-	def get_by_id(self, collection: str, id: int):
-		try:
-			project: dict = self.db.table(collection).get(doc_id=id)
-			return project
-		except:
-			return None
+    def get_by_id(self, collection: str, project_id: int) -> dict:
+        try:
+            project: dict = self.db.table(collection).get(doc_id=project_id)
+            return project
+        except:
+            return None
 
-	def update(self, collection: str, data: dict, id: int):
-		try:
-			self.db.table(collection).update(data, doc_ids=[id])
-			return True
-		except:
-			return False
+    def update(self, collection: str, data: dict, project_id: int) -> bool:
+        try:
+            self.db.table(collection).update(data, doc_ids=[project_id])
+            return True
+        except:
+            return False
 
-	def delete(self, collection: str, id: int):
-		try:
-			self.db.table(collection).remove(doc_ids=[id])
-			return True
-		except:
-			return False
+    def delete(self, collection: str, project_id: int) -> bool:
+        try:
+            self.db.table(collection).remove(doc_ids=[project_id])
+            return True
+        except:
+            return False
